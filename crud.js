46a0fd83127eedf33e9a75db5302ecc90e3dfd2f
semia <tuvulukidi@gmail.com>
2020-05-07 12:41:59 +0200

    verbe http

--- crud.js
+++ crud.js
@@ -1,4 +1,5 @@
 const formulaire = document.querySelector("#formulaire");
+const _id = document.querySelector("#_id");
 const nom = document.querySelector("#nom");
 const prenom = document.querySelector("#prenom");
 const email = document.querySelector("#email");
@@ -10,11 +11,60 @@ const tbody = document.querySelector("tbody");
 const btnSubmit = document.querySelector("#submit");
 const btnReset = document.querySelector("#reset");
 let updateRow;
+// ----------
+// API
+//--------------
+// Afficher les employés
+recuperEmployes();
+function recuperEmployes() {
+    tbody.innerHTML = "";
+  axios
+    .get("http://167.71.45.243:4000/api/employes/?api_key=svutqyq")
+    .then(function (response) {
+      for (const employe of response.data) {
+        Afficheremployes(arrangerAttributEmploye(employe));
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function arrangerAttributEmploye(employe) {
+  return {
+    nom: employe.nom,
+    prenom: employe.prenom,
+    email: employe.email,
+    poste: employe.poste,
+    numeroTelephone:
+      employe.numeroTelephone == undefined ? "" : employe.numeroTelephone,
+    estMarie: employe.estMarie,
+    pays: employe.pays,
+    _id: employe._id,
+
+  };
+}
+
+function Afficheremployes(employe) {
+  const newTr = document.createElement("tr");
+  // boucle pour les td
+  for (const attribut in employe) {
+    if (employe.hasOwnProperty(attribut)) {
+      const newTd = document.createElement("td");
+      newTd.innerHTML = employe[attribut];
+      newTr.appendChild(newTd);
+    }
+  }
+  newTr.cells[7].style.display = "none";
+  newTr.innerHTML += ajouterBouttons(employe._id);
+  tbody.appendChild(newTr);
+    ajouterEvenement(employe._id);
+}
 
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
   //   validation du formulaire
-  
+
   if (validerFormulaire()) {
     const objectEmploye = {
       nom: nom.value,
@@ -25,33 +75,35 @@ btnSubmit.addEventListener("click", function (e) {
       estMarie: statut.value,
       pays: pays.value,
     };
-    let newTr;
     if (btnSubmit.value == "Modifier") {
-        //  on recupere  le tr à modifier
-        newTr = updateRow;
-        newTr.innerHTML = "";
-        btnSubmit.value = "Ajouter"
-    }else{
-        // on cree une nouvelle ligne
-        newTr = document.createElement('tr');
+      //PUT
+      axios
+        .put(`http://167.71.45.243:4000/api/employes/${_id.value}?api_key=svutqyq`,objectEmploye)
+        .then(function (response) {
+            btnSubmit.value = "Ajouter"
+            recuperEmployes()
+          
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      //POST
+      axios
+        .post("http://167.71.45.243:4000/api/employes/?api_key=svutqyq",objectEmploye)
+        .then(function (response) {
+            recuperEmployes()
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-    // boucle pour les td
-    for (const attribut in objectEmploye) {
-      if (objectEmploye.hasOwnProperty(attribut)) {
-        const newTd = document.createElement("td");
-        newTd.innerHTML =  objectEmploye[attribut];
-        newTr.appendChild(newTd);
-      }
-    }
-    newTr.innerHTML += ajouterBouttons();
-    tbody.appendChild(newTr);
-    ajouterEvenement();
     formulaire.reset();
   }
 });
-reset.addEventListener("click",function(e){
-    e.preventDefault();
-    formulaire.reset();
+reset.addEventListener("click", function (e) {
+  e.preventDefault();
+  formulaire.reset();
 });
 function validerFormulaire() {
   const erreurNom = document.querySelector("#erreurnom");
@@ -96,7 +148,7 @@ function validerFormulaire() {
   } else {
     erreurPays.innerText = "";
   }
-  
+
   if (
     !erreurNom.innerText.length &&
     !erreurPrenom.innerText.length &&
@@ -106,47 +158,55 @@ function validerFormulaire() {
     !erreurStatut.innerText.length &&
     !erreurPays.innerText.length
   ) {
-      return true
-  }else{
-      return false
+    return true;
+  } else {
+    return false;
   }
 }
-function ajouterBouttons(){
-    return `<td>
+function ajouterBouttons(id) {
+  return `<td>
                 <button class="ui button btn-modifier">Modifier</button>
-                <button class="ui button btn-supprimer">Supprimer</button>
+                <button class="ui button btn-supprimer" id="btn-supprimer-${id}">Supprimer</button>
             <td>
             `;
 }
 
-function ajouterEvenement(){
-    const btnModifier = document.querySelectorAll('.btn-modifier');
-    const btnSupprimer = document.querySelectorAll('.btn-supprimer');
+function ajouterEvenement(id) {
+  const btnModifier = document.querySelectorAll(".btn-modifier");
+  const btnSupprimer = document.querySelector(`#btn-supprimer-${id}`);
 
-    //modification
-    for (let index = 0; index < btnModifier.length; index++) {
-        btnModifier[index].addEventListener("click",function(e){
-            btnSubmit.value = "Modifier";
-            let row = e.target.parentNode.parentNode;
-            updateRow = row ;
-            // on rempli le formulaire 
-            nom.value = row.cells[0].innerHTML;
-            prenom.value = row.cells[1].innerHTML;
-            email.value = row.cells[2].innerHTML;
-            poste.value = row.cells[3].innerHTML;
-            telephone.value = row.cells[4].innerHTML;
-            statut.value = row.cells[5].innerHTML;
-            pays.value = row.cells[6].innerHTML;
+  //modification
+  for (let index = 0; index < btnModifier.length; index++) {
+    btnModifier[index].addEventListener("click", function (e) {
+      btnSubmit.value = "Modifier";
+      let row = e.target.parentNode.parentNode;
+      updateRow = row;
+      // on rempli le formulaire
+      nom.value = row.cells[0].innerHTML;
+      prenom.value = row.cells[1].innerHTML;
+      email.value = row.cells[2].innerHTML;
+      poste.value = row.cells[3].innerHTML;
+      telephone.value = row.cells[4].innerHTML;
+      statut.value = row.cells[5].innerHTML;
+      pays.value = row.cells[6].innerHTML;
+      _id.value = row.cells[7].innerHTML;
+    });
+  }
+  // Suppression
+    btnSupprimer.addEventListener("click", function(e) {
+      if (confirm("Voulez-vous vraiment supprimer cet employé ???")) {
+        const tr = e.target.parentNode.parentNode;
+        const id = tr.cells[7].innerText;
+        axios
+        .delete(`http://167.71.45.243:4000/api/employes/${id}?api_key=svutqyq`)
+        .then(function (response) {
+            btnSubmit.value = "Ajouter"
+            recuperEmployes()
+          
+        })
+        .catch(function (error) {
+          console.log(error);
         });
-        
-    }
-    // Suppression
-    for (let index = 0; index < btnSupprimer.length; index++) {
-        btnSupprimer[index].addEventListener("click",function(e){
-            if(confirm('Voulez-vous vraiment supprimer cet employé ???')){
-                const tr = e.target.parentNode.parentNode;
-                tbody.removeChild(tr);
-            }
-        });
-    }
+      }
+    });
 }
