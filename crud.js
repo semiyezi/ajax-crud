59f2d879ae4cd704eb3e99d2203c7d451ec4b46c
semia <tuvulukidi@gmail.com>
2020-05-07 12:28:25 +0200

    validation

--- crud.js
+++ crud.js
@@ -11,26 +11,88 @@ const btnSubmit = document.querySelector("#submit");
 
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-  const objectEmploye = {
-    nom: nom.value,
-    prenom: prenom.value,
-    email: email.value,
-    numeroTelephone: telephone.value,
-    poste: poste.value,
-    estMarie: statut.value,
-    pays: pays.value,
-  };
+  //   validation du formulaire
+  
+  if (validerFormulaire()) {
+    const objectEmploye = {
+      nom: nom.value,
+      prenom: prenom.value,
+      email: email.value,
+      numeroTelephone: telephone.value,
+      poste: poste.value,
+      estMarie: statut.value,
+      pays: pays.value,
+    };
 
-  //  on ajoute un tr dans le tbody avec les données de l'object
-  const newTr = document.createElement("tr");
-  // boucle pour les td
-  for (const attribut in objectEmploye) {
-    if (objectEmploye.hasOwnProperty(attribut)) {
-      const newTd = document.createElement("td");
-      newTd.innerText = objectEmploye[attribut];
-      newTr.appendChild(newTd);
+    //  on ajoute un tr dans le tbody avec les données de l'object
+    const newTr = document.createElement("tr");
+    // boucle pour les td
+    for (const attribut in objectEmploye) {
+      if (objectEmploye.hasOwnProperty(attribut)) {
+        const newTd = document.createElement("td");
+        newTd.innerText = objectEmploye[attribut];
+        newTr.appendChild(newTd);
+      }
     }
+    tbody.appendChild(newTr);
+    formulaire.reset();
   }
-  tbody.appendChild(newTr);
-  formulaire.reset();
 });
+function validerFormulaire() {
+  const erreurNom = document.querySelector("#erreurnom");
+  const erreurPrenom = document.querySelector("#erreurprenom");
+  const erreurEmail = document.querySelector("#erreuremail");
+  const erreurPoste = document.querySelector("#erreurposte");
+  const erreurTelephone = document.querySelector("#erreurtelephone");
+  const erreurStatut = document.querySelector("#erreurstatut");
+  const erreurPays = document.querySelector("#erreurpays");
+  if (!nom.value.length) {
+    erreurNom.innerText = "Le nom est requis";
+  } else {
+    erreurNom.innerHTML = "";
+  }
+  if (!prenom.value.length) {
+    erreurPrenom.innerText = "Le Prenom est requis";
+  } else {
+    erreurPrenom.innerText = "";
+  }
+  if (!email.value.length) {
+    erreurEmail.innerText = "Le Email est requis";
+  } else {
+    erreurEmail.innerText = "";
+  }
+  if (!poste.value.length) {
+    erreurPoste.innerText = "Le Poste est requis";
+  } else {
+    erreurPoste.innerText = "";
+  }
+  if (!telephone.value.length) {
+    erreurTelephone.innerText = "Le Telephone est requis";
+  } else {
+    erreurTelephone.innerText = "";
+  }
+  if (!statut.value.length) {
+    erreurStatut.innerText = "Le Statut est requis";
+  } else {
+    erreurStatut.innerText = "";
+  }
+  if (!pays.value.length) {
+    erreurPays.innerText = "Le Pays est requis";
+  } else {
+    erreurPays.innerText = "";
+  }
+  
+  if (
+    !erreurNom.innerText.length &&
+    !erreurPrenom.innerText.length &&
+    !erreurEmail.innerText.length &&
+    !erreurPoste.innerText.length &&
+    !erreurTelephone.innerText.length &&
+    !erreurStatut.innerText.length &&
+    !erreurPays.innerText.length
+  ) {
+      return true
+  }else{
+      return false
+  }
+}
