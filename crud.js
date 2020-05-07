be7763f4bbb6a8722c55c3b1be96b3cd046d374e
semia <tuvulukidi@gmail.com>
2020-05-07 12:34:29 +0200

    ajout bouton

--- crud.js
+++ crud.js
@@ -8,6 +8,8 @@ const statut = document.querySelector("#statutmarital");
 const pays = document.querySelector("#paysorigine");
 const tbody = document.querySelector("tbody");
 const btnSubmit = document.querySelector("#submit");
+const btnReset = document.querySelector("#reset");
+let updateRow;
 
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
@@ -23,20 +25,33 @@ btnSubmit.addEventListener("click", function (e) {
       estMarie: statut.value,
       pays: pays.value,
     };
-
-    //  on ajoute un tr dans le tbody avec les données de l'object
-    const newTr = document.createElement("tr");
+    let newTr;
+    if (btnSubmit.value == "Modifier") {
+        //  on recupere  le tr à modifier
+        newTr = updateRow;
+        newTr.innerHTML = "";
+        btnSubmit.value = "Ajouter"
+    }else{
+        // on cree une nouvelle ligne
+        newTr = document.createElement('tr');
+    }
     // boucle pour les td
     for (const attribut in objectEmploye) {
       if (objectEmploye.hasOwnProperty(attribut)) {
         const newTd = document.createElement("td");
-        newTd.innerText = objectEmploye[attribut];
+        newTd.innerHTML =  objectEmploye[attribut];
         newTr.appendChild(newTd);
       }
     }
+    newTr.innerHTML += ajouterBouttons();
     tbody.appendChild(newTr);
+    ajouterEvenement();
     formulaire.reset();
   }
+});
+reset.addEventListener("click",function(e){
+    e.preventDefault();
+    formulaire.reset();
 });
 function validerFormulaire() {
   const erreurNom = document.querySelector("#erreurnom");
@@ -95,4 +110,43 @@ function validerFormulaire() {
   }else{
       return false
   }
+}
+function ajouterBouttons(){
+    return `<td>
+                <button class="ui button btn-modifier">Modifier</button>
+                <button class="ui button btn-supprimer">Supprimer</button>
+            <td>
+            `;
+}
+
+function ajouterEvenement(){
+    const btnModifier = document.querySelectorAll('.btn-modifier');
+    const btnSupprimer = document.querySelectorAll('.btn-supprimer');
+
+    //modification
+    for (let index = 0; index < btnModifier.length; index++) {
+        btnModifier[index].addEventListener("click",function(e){
+            btnSubmit.value = "Modifier";
+            let row = e.target.parentNode.parentNode;
+            updateRow = row ;
+            // on rempli le formulaire 
+            nom.value = row.cells[0].innerHTML;
+            prenom.value = row.cells[1].innerHTML;
+            email.value = row.cells[2].innerHTML;
+            poste.value = row.cells[3].innerHTML;
+            telephone.value = row.cells[4].innerHTML;
+            statut.value = row.cells[5].innerHTML;
+            pays.value = row.cells[6].innerHTML;
+        });
+        
+    }
+    // Suppression
+    for (let index = 0; index < btnSupprimer.length; index++) {
+        btnSupprimer[index].addEventListener("click",function(e){
+            if(confirm('Voulez-vous vraiment supprimer cet employé ???')){
+                const tr = e.target.parentNode.parentNode;
+                tbody.removeChild(tr);
+            }
+        });
+    }
 }
